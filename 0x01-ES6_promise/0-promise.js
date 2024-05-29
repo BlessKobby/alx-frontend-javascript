d9da52a1dd5660a0f57e1a250fd2e8e4287bc0b7
BlessKobby <amablebless@gmail.com>
2024-05-29 00:24:27 +0000

    ES6_Promise.

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 // Return a Promise using this prototype function getResponseFromAPI().
 // eslint-disable.
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {});
+  return new Promise(() => {});
 }
